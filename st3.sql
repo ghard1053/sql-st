76c0a41f67afdf14c8905e87831e1cb067429b92
Takao <sukisya70@gmail.com>
2019-02-06 23:59:03 +0900

    190206

--- st3.sql
+++ st3.sql
@@ -26,3 +26,14 @@ delete from Products P1
                     from Products P2
                     where P1.name = P2.name
                     and P1.price = p2.price);
+
+select distinct A1.name, A1.address
+  from Addresses A1 inner join Addresses A2
+  on A1.family_id = A2.family_id
+  and A1.address <> A2.address;
+
+select distinct p1.name, p1.price
+  from Products p1 inner join Products p2
+  on p1.price = p2.price
+  and p1.name <> p2.name
+  order by p1.price;
