aeeb93ef7d1c1f8ef5c37330d504b0384651e6e5
Takao <sukisya70@gmail.com>
2019-03-07 23:38:58 +0900

    190307

--- st8.sql
+++ st8.sql
@@ -1,4 +1,4 @@
--- 集合演算 
+-- 集合演算
 
 select count(*) as row_cnt
 from (
@@ -42,3 +42,37 @@ where not exists
    select skill
    from EmpSkills ES2
    where ES1.emp = ES2.emp);
+
+-- 等しい部分集合
+select SP1.sup as s1, SP2.sup as s2
+from SupParts SP1, SupParts SP2
+where SP1.sup < SP2.sup
+and SP1.part = SP2.part
+group by SP1.sup, SP2.sup
+having count(*) = (
+  select count(*)
+  from SupParts SP3
+  where SP3.sup = SP1.sup
+) and count(*) = (
+  select count(*)
+  from SupParts SP4
+  where SP4.sup = SP2.sup
+);
+
+-- 重複行を削除する高速なクエリ
+delete from Products
+where rowid in (
+  select rowid
+  from Products
+  except
+  select max(rowid)
+  from Products
+  group by name, price
+);
+
+delete from Products
+where rowid not in (
+  select max(rowid)
+  from Products
+  group by name, price
+);
