6ab492594b579405df4025784bee1051a7776543
Takao <sukisya70@gmail.com>
2019-03-09 14:42:06 +0900

    190309

--- st9.sql
+++ st9.sql
@@ -79,3 +79,50 @@ from (
 ) TMP
 where end_seat - seat = (:head_cnt - 1);
 
+
+select deal_date, 
+       price, 
+       case sign(
+         price - max(price)
+                   over(
+                     order by deal_date
+                     rows between 1 preceding
+                     and 1 preceding
+                   )
+       )
+       when 1 then 'up'
+       when 0 then 'stay'
+       when -1 then 'down' else null end as diff
+from MyStock;
+
+create VIEW MyStockUpSeq(deal_date, price, row_num) as
+select deal_date, price, row_num
+from (
+  select deal_date, 
+        price, 
+        case sign(
+          price - max(price)
+                    over(
+                      order by deal_date
+                      rows between 1 preceding
+                      and 1 preceding
+                    )
+        )
+        when 1 then 'up'
+        when 0 then 'stay'
+        when -1 then 'down' else null end as diff,
+        row_number() over(order by deal_date) as row_num
+  from MyStock
+) TMP
+where diff = 'up';
+
+select min(deal_date) as start_date,
+       '~',
+       max(deal_date) as end_date
+from (select M1.deal_date,
+             count(m2.row_num) - min(M1.row_num) as gap
+      from MyStockUpSeq M1 inner join MyStockUpSeq M2
+      on M2.row_num <= M1.row_num
+      group by M1.deal_date) TMP
+group by gap;
+
