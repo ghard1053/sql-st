83656c52ce3b7a25c62b3feaab7f8cf8b5cd0749
Takao <sukisya70@gmail.com>
2019-05-06 20:50:59 +0900

    190506

--- sql_st2/st8.sql
+++ sql_st2/st8.sql
@@ -1,18 +1,93 @@
 
--- Window
+-- WINDOW
 
-SELECT shohin_mai, shohin_bunrui, hanbai_tanka,
+SELECT shohin_mei, shohin_bunrui, hanbai_tanka,
        RANK () OVER (
          PARTITION BY shohin_bunrui
          ORDER BY hanbai_tanka
        ) AS ranking
 FROM Shohin;
 
+
 --- DENSE_RANK
 --- ROW_NUMBER
 
-SELECT shohin_mai, shohin_bunrui, hanbai_tanka,
+SELECT shohin_mei, shohin_bunrui, hanbai_tanka,
        RANK () OVER (ORDER BY hanbai_tanka) AS ranking,
        DENSE_RANK () OVER (ORDER BY hanbai_tanka) AS dense_ranking,
        ROW_NUMBER () OVER (ORDER BY hanbai_tanka) AS row_num
 FROM Shohin;
+
+
+--- moving average
+
+SELECT shohin_id, shohin_mei, hanbai_tanka,
+       AVG (hanbai_tanka) OVER (ORDER BY shohin_id
+                                ROWS 2 PRECEDING) AS moving_avg
+FROM Shohin;
+
+SELECT shohin_id, shohin_mei, hanbai_tanka,
+       AVG (hanbai_tanka) OVER (ORDER BY shohin_id
+                                ROWS BETWEEN 1 PRECEDING
+                                AND 1 FOLLOWING) AS moving_avg
+FROM Shohin;
+
+
+SELECT shohin_mei, shohin_bunrui, hanbai_tanka,
+       RANK () OVER (ORDER BY hanbai_tanka) AS ranking
+FROM Shohin
+ORDER BY ranking;
+
+
+
+
+-- GROUPING
+
+--- ROLLUP
+
+SELECT shohin_bunrui, SUM(hanbai_tanka) AS sum_tanka
+FROM Shohin
+GROUP BY ROLLUP(shohin_bunrui);
+
+SELECT shohin_bunrui, torokubi, SUM(hanbai_tanka) AS sum_tanka
+FROM Shohin
+GROUP BY ROLLUP(shohin_bunrui, torokubi);
+
+
+--- GROUPING
+
+SELECT CASE WHEN GROUPING(shohin_bunrui) = 1
+            THEN '商品分類 合計'
+            ELSE shohin_bunrui END AS shohin_bunrui,
+       CASE WHEN GROUPING(torokubi) = 1
+            THEN '登録日 合計'
+            ELSE CAST(torokubi AS VARCHAR(16)) END AS torokubi,
+       SUM(hanbai_tanka) AS sum_tanka
+FROM Shohin
+GROUP BY ROLLUP(shohin_bunrui, torokubi);
+
+
+--- CUBE
+
+SELECT CASE WHEN GROUPING(shohin_bunrui) = 1
+            THEN '商品分類 合計'
+            ELSE shohin_bunrui END AS shohin_bunrui,
+       CASE WHEN GROUPING(torokubi) = 1
+            THEN '登録日 合計'
+            ELSE CAST(torokubi AS VARCHAR(16)) END AS torokubi,
+       SUM(hanbai_tanka) AS sum_tanka
+FROM Shohin
+GROUP BY CUBE(shohin_bunrui, torokubi);
+
+
+--- GROUPING SETS
+
+SELECT CASE WHEN GROUPING(shohin_bunrui) = 1
+            THEN '商品分類 合計'
+            ELSE shohin_bunrui END AS shohin_bunrui,
+       CASE WHEN GROUPING(torokubi) = 1
+            THEN '登録日 合計'
+            ELSE CAST(torokubi AS VARCHAR(16)) END AS torokubi,
+       SUM(hanbai_tanka) AS sum_tanka
+FROM Shohin
+GROUP BY GROUPING SETS (shohin_bunrui, torokubi);
